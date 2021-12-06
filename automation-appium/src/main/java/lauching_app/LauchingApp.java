a5f397b50cbfb2cccb4c6da20e4dbc423d0b0d2e
Xuyen Chi <xuyenltn1996@gmail.com>
2021-12-06 16:38:34 +0700

    first lauching

---
+++ automation-appium/src/main/java/lauching_app/LauchingApp.java
@@ -0,0 +1,10 @@
+package lauching_app;
+
+public class LauchingApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ automation-appium/src/main/java/lauching_app/MobileCapapilityTypeEx.java
@@ -0,0 +1,8 @@
+package lauching_app;
+
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public interface MobileCapapilityTypeEx extends MobileCapabilityType{
+    String PLATFORM_NAME = "platformName";
+    String AUTOMATION_NAME = "automationName";
+}
