2ba9388fd0ec99fa458cd258244dd4770f1763f8
Xuyen Chi <xuyenltn1996@gmail.com>
2021-12-07 11:57:14 +0700

    first lauching

--- automation-appium/src/main/java/lauching_app/MobileCapapilityTypeEx.java
+++ automation-appium/src/main/java/lauching_app/MobileCapapilityTypeEx.java
@@ -2,7 +2,10 @@ package lauching_app;
 
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public interface MobileCapapilityTypeEx extends MobileCapabilityType{
-    String PLATFORM_NAME = "platformName";
-    String AUTOMATION_NAME = "automationName";
+public interface MobileCapapilityTypeEx extends MobileCapabilityType {
+	String PLATFORM_NAME = "platformName";
+	String AUTOMATION_NAME = "automationName";
+	String UDID = "udid";
+	String APP_PACKSGE = "appPackage";
+	String APP_ACTIVITY = "appActivity";
 }
